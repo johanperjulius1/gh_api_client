2620d3595ceb613af30bea1ee00e43851201f05c
johanperjulius1 <johan.per.julius.berglund@gmail.com>
2020-10-01 11:21:02 +0200

    creating user story and from that adding to the spec file.

--- cypress/integration/userCanSeeApplicationTitle.feature.js
+++ cypress/integration/userCanSeeApplicationTitle.feature.js
@@ -6,5 +6,15 @@ describe('Application main view', () => {
   it('contains titel', () => {
     cy.get("section[name='title']")
       .should('contain', 'GitHub Search engine')
+  });    
+  
+  it('User can make a seach')
+  cy.get('input').type(johanperjulius1);
+  cy.route({
+    method: "GET",
+    url: "http://localhost:3000/",
+    response: "fixture:githubSearchResponse.json",
   });
+
+  
 });
